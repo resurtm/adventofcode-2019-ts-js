baf35e94f8f2644bf30efb0953291acb349d415a
resurtm <resurtm@gmail.com>
2019-12-06 14:52:38 +0100

    add eslint for typescript

--- ts/day01.ts
+++ ts/day01.ts
@@ -1,19 +1,20 @@
 import { readFile as readFileOriginal } from 'fs';
+import { join } from 'path';
 import { promisify } from 'util';
 import { add, filter, map, reduce } from 'ramda';
 
 const readFile = promisify(readFileOriginal);
 const calculateFuel = (weight: number): number => Math.floor(weight / 3) - 2;
 
-async function run(): Promise<void> {
-    const data: string = (await readFile(__dirname + '/../input/day01.txt')).toString();
+async function run (): Promise<void> {
+    const data: string = (await readFile(join(__dirname, '..', 'input', 'day01.txt'))).toString();
     const items: number[] = map(
         str => parseInt(str, 10),
-        data.split('\n'),
+        data.split('\n')
     );
     const fuel: number[] = map(
         weight => calculateFuel(weight),
-        filter<number>(x => !Number.isNaN(x), items),
+        filter<number>(x => !Number.isNaN(x), items)
     );
     const totalFuel: number = reduce<number, number>(add, 0, fuel);
     console.log(`day 01: ${totalFuel}`);
