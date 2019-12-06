1116c9996743ed05884b995ae2eee57dc805293d
resurtm <resurtm@gmail.com>
2019-12-06 15:46:12 +0100

    day 01, part 2

--- ts/day01.ts
+++ ts/day01.ts
@@ -4,20 +4,42 @@ import { promisify } from 'util';
 import { add, filter, map, reduce } from 'ramda';
 
 const readFile = promisify(readFileOriginal);
+
 const calculateFuel = (weight: number): number => Math.floor(weight / 3) - 2;
+
+function calculateWithFuelWeight (inputWeight: number): number {
+    let result = 0;
+    let weight = inputWeight;
+    while (weight > 0) {
+        weight = calculateFuel(weight);
+        result += weight;
+    }
+    return result;
+}
 
 async function run (): Promise<void> {
     const data: string = (await readFile(join(__dirname, '..', 'input', 'day01.txt'))).toString();
-    const items: number[] = map(
-        str => parseInt(str, 10),
-        data.split('\n')
+    const weights: number[] = filter(
+        x => !Number.isNaN(x),
+        map(
+            str => parseInt(str, 10),
+            data.split('\n')
+        )
     );
-    const fuel: number[] = map(
-        weight => calculateFuel(weight),
-        filter<number>(x => !Number.isNaN(x), items)
+
+    const totalFuel1: number = reduce<number, number>(
+        add,
+        0,
+        map(weight => calculateFuel(weight), weights)
     );
-    const totalFuel: number = reduce<number, number>(add, 0, fuel);
-    console.log(`day 01: ${totalFuel}`);
+    console.log(`day 01: ${totalFuel1}`);
+
+    const totalFuel2: number = reduce<number, number>(
+        add,
+        0,
+        map(weight => calculateWithFuelWeight(weight), weights)
+    );
+    console.log(`day 02: ${totalFuel2}`);
 }
 
 run();
